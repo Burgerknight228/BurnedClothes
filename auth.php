320f55a87ea3116431dc9eb9eea7fd47d32f6b1c
Burgerknight228 <snissarroman@gmail.com>
2024-11-10 21:30:36 +0500

    sqlConnect

--- auth.php
+++ auth.php
@@ -8,13 +8,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="indexStyle.css">
+    <link rel="stylesheet" href="authStyle.css">
 </head>
 <body>
     <header>
         <?php require "blocks/header.php" ?>
     </header>
     <main>
-        <h1>Вы не зарегистрированы</h1>
+    <div class="register-conteiner">
+        <h1>Регистрация</h1>
+            <form action="check.php" method="post">
+                <input type="text" class="form-control" name="login" id="login" placeholder="Введите логин">
+                <input type="password" class="form-control" name="pass" id="pass" placeholder="Введите пароль">
+                <input type="text" class="form-control" name="name" id="name" placeholder="Введите имя">
+                <button class="form-btn">Зарегистрироваться</button>
+            </form>
+    </div>
     </main>
     <footer>
         <?php require "blocks/footer.php" ?>
---
+++ check.php
@@ -0,0 +1,22 @@
+<?php
+    $login = htmlspecialchars(trim($_POST['login']), ENT_QUOTES);
+    $pass = htmlspecialchars(trim($_POST['pass']),ENT_QUOTES);
+    $name = htmlspecialchars(trim($_POST['name']),ENT_QUOTES);
+
+    if(mb_strlen($login) < 5 || mb_strlen($login)>20){
+        echo "Недопустимая длина логина";
+        exit();
+    }else if(mb_strlen($pass) < 2 || mb_strlen($pass)>32){
+        echo "Недопустимая длина пароля";
+        exit();
+    }else if(mb_strlen($name) < 3 || mb_strlen($name)>20){
+        echo "Недопустимая длина имени";
+        exit();
+    }
+
+    $mysql = new mysqli('MySQL-8.2', 'root', '', 'BurnedClothesDB');
+    $mysql ->query("INSERT INTO `users` (`login`, `pass`, `name`)
+    VALUES('$login', '$pass', '$name')");
+
+    $mysql->close();
+?>
